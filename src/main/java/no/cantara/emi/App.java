5457efbaa11086578d07fa059654903875097af4
baardl <bard.lind@gmail.com>
2016-05-25 10:05:03 +0200

    Functional test on windows code.

--- src/main/java/no/cantara/emi/App.java
+++ src/main/java/no/cantara/emi/App.java
@@ -3,10 +3,28 @@ package no.cantara.emi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
+        String[] command = {"cmd.exe", "/c", "net", "stop", "java-auto-update"};
+        command = new String[]{"bin/java-auto-update.bat", "stop"};
+        try {
+            Process process = new ProcessBuilder(command).start();
+            InputStream inputStream = process.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(Exception ex) {
+            System.out.println("Exception : "+ex);
+        }
     }
 
 }
