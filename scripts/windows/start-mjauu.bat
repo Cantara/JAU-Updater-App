ce7e16d780874d40a0539874517fffad5e94342a
baardl <bard.lind@gmail.com>
2016-05-26 13:50:29 +0200

    Added windows script for start MJAUU

---
+++ scripts/windows/start-mjauu.bat
@@ -0,0 +1,4 @@
+echo %date% - %time% Start MJAUU  >> mjauu-win.log
+start /b javaw -jar jau-updater-app-0.8-beta-5-SNAPSHOT.jar >> mjauu-app-win.log
+echo %date% - %time% Done >> mjauu-win.log
+exit
