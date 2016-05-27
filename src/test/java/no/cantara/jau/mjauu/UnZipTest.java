9a6a3a0dc674693c6b15c89cb3ab6e0b6371a620
baardl <bard.lind@gmail.com>
2016-05-27 15:56:35 +0200

    ZIp extractor

--- src/test/java/no/cantara/jau/mjauu/UnZipTest.java
+++ src/test/java/no/cantara/jau/mjauu/UnZipTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.net.URI;
 import java.net.URL;
 
 import static org.testng.Assert.assertTrue;
@@ -29,11 +30,21 @@ public class UnZipTest {
     @Test
     public void testUnzip() throws Exception {
         URL zipSource = this.getClass().getClassLoader().getResource("jau-updater-test.zip");
-        File zipFile = new File("C:\\sources\\JAU-Updater-App\\target\\test-classes\\jau-updater-test.zip"); //new File(zipSource.toString());
+        URI zipUri = new URI(zipSource.toString());
+        File zipFile = new File(zipUri);//new File("C:\\sources\\JAU-Updater-App\\target\\test-classes\\jau-updater-test.zip"); //new File(zipSource.toString());
         assertTrue(zipFile.exists(),"Test zip not found " + zipSource.toString());
         String toDirName = "C:\\sources\\JAU-Updater-App\\target\\test-classes\\tmp"; //zipFile.getPath() +"/../tmp"
-        File toDir = new File(toDirName);
+        File toDir = new File(zipFile.getParentFile().toString() + File.separator + "tmp");
         unZip.extract(zipFile, toDir);
+        String expectToFind = Main.class.getCanonicalName();
+        expectToFind = expectToFind.replace(".", File.separator) + ".java";
+
+        //URI expectedUri = new URI(toDir.toString()+ File.separator + "main" + File.separator + "java" + File.separator + expectToFind);
+        //assertNotNull(expectedUri);
+        File expectedFile = new File(toDir.toString()+ File.separator + "main" + File.separator + "java" + File.separator + expectToFind);
+        assertTrue(expectedFile.exists());
+
+
 
     }
 
