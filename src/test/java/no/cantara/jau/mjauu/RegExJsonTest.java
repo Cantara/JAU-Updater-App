a1a2fa5123bd5070880fe77add7fef823e1b12fa
baardl <bard.lind@gmail.com>
2016-06-13 08:22:36 +0200

    JsonRegex test

--- src/test/java/no/cantara/jau/mjauu/RegExJsonTest.java
+++ src/test/java/no/cantara/jau/mjauu/RegExJsonTest.java
@@ -21,7 +21,7 @@ public class RegExJsonTest {
     @Test
     public void findAgentIdFromJson() throws IOException, URISyntaxException {
         String regex = "\\\"agentId\\\"\\:\\\"[0-9]*\\\"";
-        String validJson = "";
+        String validJson = ""; //TODO validate as json
         String exampleLog = "06:17:39.325 [pool-3-thread-1] INFO  n.n.p.s.File - some data data as json={\"agentId\":\"8011\",";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(exampleLog);
