203de841137e09dbb77b28c86ce2149b45ad9cea
baardl <bard.lind@gmail.com>
2016-06-13 08:33:26 +0200

    Validating JsonRegex ok.

--- src/test/java/no/cantara/jau/mjauu/RegExJsonTest.java
+++ src/test/java/no/cantara/jau/mjauu/RegExJsonTest.java
@@ -1,5 +1,6 @@
 package no.cantara.jau.mjauu;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -18,14 +19,30 @@ import static org.testng.AssertJUnit.assertTrue;
 public class RegExJsonTest {
     private static final Logger log = LoggerFactory.getLogger(RegExJsonTest.class);
 
+    private final String exampleLog = "06:17:39.325 [pool-3-thread-1] INFO  n.n.p.s.File - some data data as json={\"agentId\":\"8011\",";
+
     @Test
-    public void findAgentIdFromJson() throws IOException, URISyntaxException {
+    public void findAgentIdFromRegex() throws IOException, URISyntaxException {
         String regex = "\\\"agentId\\\"\\:\\\"[0-9]*\\\"";
-        String validJson = ""; //TODO validate as json
-        String exampleLog = "06:17:39.325 [pool-3-thread-1] INFO  n.n.p.s.File - some data data as json={\"agentId\":\"8011\",";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(exampleLog);
         assertTrue(m.find());
         assertEquals(m.group(),"\"agentId\":\"8011\"");
+    }
+
+    @Test
+    public void findAgentIdFromJsonRegex() throws Exception{
+        String validJson = "{\n" +
+                "\t\"tagName\": \"pharmacyId\",\n" +
+                "\t\"regex\": \"\\\\\\\"agentId\\\\\\\"\\\\:\\\\\\\"[0-9]*\\\\\\\"\",\n" +
+                "\t\"filePath\": \"logs/pharmacyAgent.log\"\n" +
+                "}";
+        JSONObject eventExtractionConfig = new JSONObject(validJson);
+        String regex = eventExtractionConfig.getString("regex");
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(exampleLog);
+        assertTrue(m.find());
+        assertEquals(m.group(),"\"agentId\":\"8011\"");
+
     }
 }
