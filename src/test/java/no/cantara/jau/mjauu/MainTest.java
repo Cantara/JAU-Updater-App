35a03fc4d79c9cddc2c43721fa5617a22cfe0041
baardl <bard.lind@gmail.com>
2016-06-21 12:21:16 +0200

    #1 Add support for Proxy

--- src/test/java/no/cantara/jau/mjauu/MainTest.java
+++ src/test/java/no/cantara/jau/mjauu/MainTest.java
@@ -1,13 +1,11 @@
 package no.cantara.jau.mjauu;
 
-import org.testng.annotations.Test;
-
 /**
  * Created by baardl on 17/06/2016.
  */
 public class MainTest {
-    @Test
-    public void testNotifyFailure() throws Exception {
+
+    public void notifyFailure() throws Exception {
         Main main = new Main();
         main.notifyFailure("Hei", null, null);
 
